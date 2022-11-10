9f7d4778dd74f4a0b6e0cf96b87a92f2f055f728
najmiddinnazarov <najmiddinnazarov0405@gmail.com>
2022-11-10 17:07:45 +0500

    Write the "square"-function here

--- Exercises/1011/main.js
+++ Exercises/1011/main.js
@@ -4,3 +4,10 @@ function getAge(inputString) {
   return parseInt(inputString);
 }
 console.log(getAge(4));
+
+// Write the "square"-function here
+
+function square(number) {
+  return number ** 2;
+}
+console.log(square(100));
